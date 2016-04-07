c269624b885c5119e016aa94220210c8b342c43d
stephanieleclerc <stephanie.leclerc@polymtl.ca>
2016-04-07 11:33:06 -0400

    Create ExceptionEchecEmprunt.cpp

---
+++ ExceptionEchecEmprunt.cpp
@@ -0,0 +1,30 @@
+/****************************************************************************
+* Fichier: ExceptionEchecEmprunt.cpp
+* Auteur(s): Charles-Olivier Favreau et Stéphanie Leclerc
+* Date de creation: 7 avril 2016
+* Date de modification: avril 2016
+* Description: Fonction reliée à la classe ExceptionEchecEmprunt
+****************************************************************************/
+
+#include "ExceptionEchecEmprunt.h"
+#include <string>
+
+/****************************************************************************
+* Fonction: ExceptionEchecEmprunt::ExceptionEchecEmprunt
+* Description: Constructeur par paramètres
+* Paramètres: string message : un message d'erreur
+* Retour: aucun
+****************************************************************************/
+ExceptionEchecEmprunt::ExceptionEchecEmprunt(string message) {
+	compteur_++;
+}
+
+/****************************************************************************
+* Fonction: ExceptionEchecEmprunt::obtenirValeurCompteur
+* Description: Retourne compteur_
+* Paramètres: aucun
+* Retour: (int) la valeur de compteur_
+****************************************************************************/
+static int ExceptionEchecEmprunt::obtenirValeurCompteur() const {
+	return compteur_;
+}
