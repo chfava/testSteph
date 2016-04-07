e2aacaaaa5f39f2c6c1a0b740336577593938d1c
stephanieleclerc <stephanie.leclerc@polymtl.ca>
2016-04-07 11:40:50 -0400

    Modifs Exception

--- ExceptionEchecEmprunt.h
+++ ExceptionEchecEmprunt.h
@@ -1,6 +1,10 @@
 /****************************************************************************
 * Fichier: ExceptionEchecEmprunt.h
+<<<<<<< HEAD
 * Auteur(s): Charles-Olivier Favreau et Stéphanie Leclerc
+=======
+* Auteur(s): Charles-Olivier Favreau et StÃ©phanie Leclerc
+>>>>>>> c269624b885c5119e016aa94220210c8b342c43d
 * Date de creation: 7 avril 2016
 * Date de modification: avril 2016
 * Description: Description de la classe ExceptionEchecEmprunt
@@ -11,8 +15,16 @@ class ExceptionEchecEmprunt :public std::runtime_error {
 
 public:
 	ExceptionEchecEmprunt(string message);
+<<<<<<< HEAD
 	int obtenirValeurCompteur();
 
 private: 
 	static int compteur_;
 };
+=======
+	static int obtenirValeurCompteur() const;
+
+private: 
+	static int compteur_;
+};
+>>>>>>> c269624b885c5119e016aa94220210c8b342c43d
--- ExceptionEchecRetour.cpp
+++ ExceptionEchecRetour.cpp
@@ -1,16 +1,28 @@
 /****************************************************************************
 * Fichier: ExceptionEchecRetour.cpp
+<<<<<<< HEAD
 * Auteur(s): Charles-Olivier Favreau et Stéphanie Leclerc
 * Date de creation: 7 avril 2016
 * Date de modification: avril 2016
 * Description: Fonction reliée à la classe ExceptionEchecRetour
+=======
+* Auteur(s): Charles-Olivier Favreau et StÃ©phanie Leclerc
+* Date de creation: 7 avril 2016
+* Date de modification: avril 2016
+* Description: Fonction reliÃ©e Ã  la classe ExceptionEchecRetour
+>>>>>>> c269624b885c5119e016aa94220210c8b342c43d
 ****************************************************************************/
 
 #include "ExceptionEchecRetour.h"
 /****************************************************************************
 * Fonction: ExceptionEchecRetour::ExceptionEchecRetour
+<<<<<<< HEAD
 * Description: Constructeur par paramètres
 * Paramètres: string message: un message d'erreur
+=======
+* Description: Constructeur par paramÃ¨tres
+* ParamÃ¨tres: string message: un message d'erreur
+>>>>>>> c269624b885c5119e016aa94220210c8b342c43d
 * Retour: aucun
 ****************************************************************************/
 ExceptionEchecRetour::ExceptionEchecRetour(string message) :runtime_error(message) {};
