d1ff69871e1b5e17585b0c0cf6bf5cdfeea3b887
stephanieleclerc <stephanie.leclerc@polymtl.ca>
2016-04-07 11:06:33 -0400

    modifs classes Exception

--- ExceptionEchecEmprunt.cpp
+++ ExceptionEchecEmprunt.cpp
@@ -3,16 +3,28 @@
 * Auteur(s): Charles-Olivier Favreau et Stéphanie Leclerc
 * Date de creation: 7 avril 2016
 * Date de modification: avril 2016
-* Description: Fonctions reliés à la classe ExceptionEchecEmprunt
+* Description: Fonction reliée à la classe ExceptionEchecEmprunt
 ****************************************************************************/
 
 #include "ExceptionEchecEmprunt.h"
 #include <string>
 
+/****************************************************************************
+* Fonction: ExceptionEchecEmprunt::ExceptionEchecEmprunt
+* Description: Constructeur par paramètres
+* Paramètres: string message : un message d'erreur
+* Retour: aucun
+****************************************************************************/
 ExceptionEchecEmprunt::ExceptionEchecEmprunt(string message) {
 	compteur_++;
 }
 
-ExceptionEchecEmprunt::obtenirValeurCompteur() const {
+/****************************************************************************
+* Fonction: ExceptionEchecEmprunt::obtenirValeurCompteur
+* Description: Retourne compteur_
+* Paramètres: aucun
+* Retour: (int) la valeur de compteur_
+****************************************************************************/
+int ExceptionEchecEmprunt::obtenirValeurCompteur() const {
 	return compteur_;
 }
--- ExceptionEchecRetour.cpp
+++ ExceptionEchecRetour.cpp
@@ -3,10 +3,15 @@
 * Auteur(s): Charles-Olivier Favreau et Stéphanie Leclerc
 * Date de creation: 7 avril 2016
 * Date de modification: avril 2016
-* Description: Fonctions reliés à la classe ExceptionEchecRetour
+* Description: Fonction reliée à la classe ExceptionEchecRetour
 ****************************************************************************/
 
 #include "ExceptionEchecRetour.h"
-
+/****************************************************************************
+* Fonction: ExceptionEchecRetour::ExceptionEchecRetour
+* Description: Constructeur par paramètres
+* Paramètres: string message: un message d'erreur
+* Retour: aucun
+****************************************************************************/
 ExceptionEchecRetour::ExceptionEchecRetour(string message) :runtime_error(message) {};
 
